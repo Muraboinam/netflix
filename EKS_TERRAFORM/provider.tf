8892fe9a23d1b27645965c4f531fc4343761e2bf
Muraboinam <maheshmuraboinam@gmail.com>
2025-03-16 09:34:41 +0530

    Update provider.tf

--- EKS_TERRAFORM/provider.tf
+++ EKS_TERRAFORM/provider.tf
@@ -7,7 +7,7 @@ locals {
   region = "ap-south-2"
 
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["ap-south-2a"]
+  azs      = ["ap-south-2a", "ap-south-2b"]
 
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
