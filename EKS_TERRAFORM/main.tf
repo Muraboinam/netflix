5852ebbc8611b2bd1801effdc31a3d05fadf2360
Muraboinam <maheshmuraboinam@gmail.com>
2025-03-16 19:26:47 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -1,3 +1,42 @@
+locals {
+  name   = "ascode-cluster"
+  region = "ap-south-1"
+
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["ap-south-1a", "ap-south-1b"]
+
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+
+  tags = {
+    Example = local.name
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                 = "1"
+    "kubernetes.io/cluster/${local.name}"    = "owned"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"        = "1"
+    "kubernetes.io/cluster/${local.name}"    = "owned"
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
@@ -5,42 +44,46 @@ module "eks" {
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
 
-  # EKS Managed Node Group(s)
+  cluster_addons = {
+    coredns    = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni    = { most_recent = true }
+  }
+
   eks_managed_node_group_defaults = {
-    ami_type = "CUSTOM"  # Use "CUSTOM" for custom AMI
+    ami_type       = "AL2_x86_64"
     instance_types = ["t4g.large"]
     attach_cluster_primary_security_group = true
   }
 
   eks_managed_node_groups = {
-    nexflix-cluster = {
+    ascode-cluster-wg = {
       min_size     = 1
       max_size     = 2
       desired_size = 1
 
       instance_types = ["t4g.large"]
       capacity_type  = "SPOT"
-      ami_id         = "ami-00b2b099cdd31358c"  # Specify the custom AMI ID here
+      ami_id         = "ami-00b2b099cdd31358c"
 
       tags = {
-        ExtraTag = "nexflix"
+        ExtraTag = "helloworld"
       }
+    }
+  }
+
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Allow node-to-node communication"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
     }
   }
 
